ab908a6a9585dabfe8750a4634adb3420876d86f
matao <mataoshou@163.com>
2020-01-08 20:31:46 +0800

    htmlparse
    htmlparse

--- htmlParse/src/html/HtmlTask.java
+++ htmlParse/src/html/HtmlTask.java
@@ -2,6 +2,7 @@ package html;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,6 +13,7 @@ import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+import util.DownImage;
 import util.FileStore;
 
 public class HtmlTask extends Thread
@@ -22,7 +24,7 @@ public class HtmlTask extends Thread
 	{
 		try
 		{
-			getHtml("https://www.baidu.com/s?wd=%E6%B1%9F%E8%8B%8F%E7%9C%81%E8%80%83%E6%88%90%E7%BB%A9");
+			getHtml("http://www.u17.com/chapter/791220.html#image_id=5787057");
 		} catch (IOException e)
 		{
 			e.printStackTrace();
@@ -33,29 +35,26 @@ public class HtmlTask extends Thread
 	
 	public void getHtml(String url) throws IOException
 	{
-//		Document doc = Jsoup.connect(url).get();
-//		
-//		String htmlContent = doc.html();
-//		
-//		System.out.println(htmlContent);
 		
 		// HtmlUnit 模拟浏览器
-        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        WebClient webClient = new WebClient(BrowserVersion.FIREFOX_60);
         webClient.getOptions().setJavaScriptEnabled(true);              // 启用JS解释器，默认为true
         webClient.getOptions().setCssEnabled(false);                    // 禁用css支持
-        webClient.getOptions().setRedirectEnabled(true);  
+        webClient.getOptions().setRedirectEnabled(false);  
         webClient.getOptions().setThrowExceptionOnScriptError(false);   // js运行错误时，是否抛出异常
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.getOptions().setTimeout(10 * 1000);                   // 设置连接超时时间
-        HtmlPage page = webClient.getPage(url);
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());//设置支持AJAX
-        webClient.waitForBackgroundJavaScript(60 * 1000);               // 等待js后台执行30秒
-        
+                    
+        HtmlPage page = webClient.getPage(url);
+        webClient.waitForBackgroundJavaScript(60 * 1000); // 等待js后台执行30秒
         String pageAsXml = page.asXml();
+        
+        webClient.close();
         
         // Jsoup解析处理
         Document doc = Jsoup.parse(pageAsXml, url);  
-        Elements pngs = doc.select("a");                   // 获取所有图片元素集
+        Elements pngs = doc.select("img");                   // 获取所有图片元素集
         
         
         // 此处省略其他操作
@@ -65,14 +64,27 @@ public class HtmlTask extends Thread
         FileStore.putString(file, pageAsXml);
         
         System.out.println("............................." +pngs.size()); 
-        
+        DownImage down = new DownImage();
+        File root = new File(String.format("c:/111/%s/",System.currentTimeMillis()/1000));
+        root.mkdirs();
         for(int i=0;i<pngs.size();i++)
         {
-        	System.out.println(".......................................");
-        	String src = pngs.get(i).attr("href").trim();
-        	System.out.println(pngs.get(i).toString());
-        	System.out.println(src);
-        	System.out.println(".......................................");
+        	try {
+	        	System.out.println(".......................................");
+	        	String src = pngs.get(i).attr("src").trim();
+	        	System.out.println(pngs.get(i).toString());
+	        	System.out.println(src);
+	        	if(src!=null&&src.trim().length()>0)
+	        	{
+	        		down.saveToFile(src, new File(root,String.format("%s.jpg", i)));
+	        	}
+	        	
+	        	System.out.println(".......................................");
+        	}
+        	catch(Exception e)
+        	{
+        		e.printStackTrace();
+        	}
         	
         }
 	}
