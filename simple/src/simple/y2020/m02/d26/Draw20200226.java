ec77e658c5ca3bc5f2b96ca745ec935f1cb63312
mataoshou <mataoshou@163.com>
2020-02-26 22:24:16 +0800

    simple
    simple

--- simple/src/simple/y2020/m02/d26/Draw20200226.java
+++ simple/src/simple/y2020/m02/d26/Draw20200226.java
@@ -8,16 +8,34 @@ public class Draw20200226
 	String empty =" ";
 	String item = "#";
 	public void draw(int height,int count,int width)
-	{
-		int middle = width/2;
-		
+	{		
 		Random random = new Random();
+		
+		int index = width/2;
+		
+		for(int i =0;i<width;i++)
+		{
+			for(int j=0;j<height;j++)
+			{
+				if(j==index)
+				{
+					System.out.print(item+item);
+				}
+				else {
+					System.out.print(empty+empty);
+				}
+				
+			}
+			System.out.println();
+			index += random.nextInt(2)-1;
+		}
 		
 		
 	}
 	
 	public static void main(String[] args)
 	{
-		
+		Draw20200226 draw = new Draw20200226();
+		draw.draw(3, 20, 10);
 	}
 }
