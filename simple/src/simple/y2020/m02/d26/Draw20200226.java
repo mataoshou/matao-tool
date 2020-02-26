27ae17bf644d2793ff84b0981771ab3a955785ed
matao <mataoshou@163.com>
2020-02-26 18:28:26 +0800

    simple
    simple

---
+++ simple/src/simple/y2020/m02/d26/Draw20200226.java
@@ -0,0 +1,23 @@
+package simple.y2020.m02.d26;
+
+import java.util.Random;
+
+public class Draw20200226
+{
+	
+	String empty =" ";
+	String item = "#";
+	public void draw(int height,int count,int width)
+	{
+		int middle = width/2;
+		
+		Random random = new Random();
+		
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
